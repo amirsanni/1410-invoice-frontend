0a2509b92c74cb141e4c8a235351b193ed604eab
Amir Sanni <amirsanni@gmail.com>
2026-01-29 17:43:15 +0100

    Add router

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import App from '@/App';
+
+export default function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
